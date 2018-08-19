b5109ebdd142480756a40200f9efd51d2fb010a8
Mong-Kuen Sun <mongkuen.sun@gmail.com>
2018-08-19 13:05:45 -0500

    Add Prettier, Reload server on filechange

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,11 +1,14 @@
 var path = require('path')
-var nodeExternals = require('webpack-node-externals');
+var nodeExternals = require('webpack-node-externals')
+var ReloadServerPlugin = require('reload-server-webpack-plugin')
+var PrettierPlugin = require("prettier-webpack-plugin")
 
 var appRoot = path.join(__dirname, '..', '..')
 
 module.exports = {
   target: 'node',
   mode: 'development',
+  watch: true,
   entry: path.join(appRoot, 'src', 'index.js'),
   output: {
     filename: 'index.js',
@@ -20,5 +23,14 @@ module.exports = {
         exclude: /node_modules/
       }
     ]
-  }
+  },
+  plugins: [
+    new ReloadServerPlugin({ script: path.join(appRoot, 'build', 'index.js') }),
+    new PrettierPlugin({
+      semi: false,
+      singleQuote: true,
+      bracketSpacing: true,
+      jsxBracketSameLine: true
+    })
+  ]
 }
